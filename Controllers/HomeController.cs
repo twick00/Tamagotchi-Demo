599e46960248553388ed11efff2d66cfdd46d9a1
Faiza Husain and Tyler Wickline <tyler@>
2018-02-07 16:27:19 -0800

    Add classes, constructors, buttons, controllers, and views

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Tamagotchi.Models;
+using static System.Console;
+
+namespace Tamagotchi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/Home")]
+        public IActionResult Home()
+        {
+            List<Tama> tamaList = Tama.GetAll();
+            return View("Home");
+        }
+        [HttpGet("/Food/{id}")]
+        public IActionResult Food(int id)
+        {
+            Tama tama = Tama.Find(id);
+            return View("Food", tama);
+        }
+        [HttpPost("/Food/{id}")]
+        public IActionResult PostFood(int id)
+        {
+            var tempTama = Request.Form["tamaId"];
+            WriteLine(tempTama);
+            Tama tama = Tama.Find(id);
+            tama.SetHunger(tama.GetHunger()-20);
+            if (tama.GetRest() < 20)
+            {
+                // TAMA DIES
+            }
+            else 
+            {
+                tama.SetRest(tama.GetRest() - 20);
+            }
+            return View("Food", tama);
+        }
+        [HttpGet("/Play/{id}")]
+        public IActionResult Play(int id)
+        {
+            Tama tama = Tama.Find(id);
+            return View("Play", tama);
+        }
+        [HttpPost("/Play/{id}")]
+        public IActionResult PostPlay(int id)
+        {
+            Tama tama = Tama.Find(id);
+            tama.SetFun(tama.GetFun()+20);
+            if (tama.GetHunger() > 80)
+            {
+                // TAMA DIES
+            }
+            else 
+            {
+                tama.SetHunger(tama.GetHunger() - 20);
+            }
+            return View("Play", tama);
+        }
+        [HttpGet("/Sleep/{id}")]
+        public IActionResult Sleep(int id)
+        {
+            Tama tama = Tama.Find(id);
+            return View("Sleep", tama);
+        }
+        [HttpPost("/Sleep/{id}")]
+        public IActionResult PostSleep(int id)
+        {
+            Tama tama = Tama.Find(id);
+            tama.SetRest(tama.GetRest()+20);
+            if (tama.GetFun() < 20)
+            {
+                // TAMA DIES
+            }
+            else 
+            {
+                tama.SetFun(tama.GetFun() - 20);
+            }
+            return View("Sleep", tama);
+        }
+        [HttpGet("/Create")]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost("/Status")]
+        public IActionResult Status()
+        {
+            Tama tama = new Tama(Request.Form["tama-name"], 50);
+            return View("Status", tama);
+        }
+        [HttpGet("/Status/{id}")]
+        public IActionResult PostStatus(int id)
+        {
+            Tama tama = Tama.Find(id);
+            return View("Status", tama);
+        }
+        [HttpGet("/Death/{id}")]
+        public IActionResult Death (int id)
+        {
+            Tama tama = Tama.Find(id);
+            return View("Death", tama);
+        }
+    }
+}
---
+++ Models/TamaVariable.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tamagotchi.Models
+{
+    public class Tama
+    {
+        private string _name;
+        private int _hunger;
+        private int _fun;
+        private int _rest;
+        private static int _id;
+        private static List<Tama> _tamas = new List<Tama> {};
+        //Below is the Tama constructor.
+        public Tama(string name, int hunger = 0, int fun = 100, int rest = 100) 
+        {
+            _name = name;
+            _hunger = hunger;
+            _fun = fun;
+            _rest = rest;
+            _tamas.Add(this);
+            _id = _tamas.Count;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public void SetHunger(int hunger)
+        {
+            _hunger = hunger;
+        }
+        public void SetFun(int fun)
+        {   
+            _fun = fun;
+        }
+        public void SetRest(int rest) 
+        {
+            _rest = rest;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetHunger()
+        {
+            return _hunger;
+        }
+        public int GetFun()
+        {
+            return _fun;
+        }
+        public int GetRest()
+        {
+            return _rest;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+        public static List<Tama> GetAll()
+        {
+         return _tamas;
+        }
+        public static void ClearAll()
+        {
+          _tamas.Clear();
+        }
+        public static Tama Find(int searchId)
+        {
+            return _tamas[searchId-1];
+        }
+
+    }
+}
